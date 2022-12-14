f08d217dd63fe10d3980fb1b83d12cdc7c443b00
Vanya Shumilo <ivan.shumilo@gmail.com>
2022-12-15 01:17:08 +0300

    modal

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import PostItem from "./components/PostItem";
 import {useMemo, useState} from "react";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/buttons/MyButton";
-import MyInput from "./components/UI/inputs/MyInput";
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
 
 
 function App() {
@@ -38,10 +37,6 @@ function App() {
         },
     ])
 
-    const createPost = (newPost) => {
-        setPosts([...posts, newPost])
-    }
-
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
@@ -60,9 +55,21 @@ function App() {
         return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
     }, [filter.query, sortedPosts])
 
+    const [visible, setVisible] = useState(false)
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+        setVisible(false)
+    }
     return (
         <div className="App">
-            <PostForm createPost={createPost}/>
+
+
+            <MyButton onClick={() => setVisible(true)}>Add Post</MyButton>
+
+            <MyModal visible={visible} setVisible={setVisible}>
+                <PostForm createPost={createPost}/>
+            </MyModal>
 
             <div className="line"></div>
 
