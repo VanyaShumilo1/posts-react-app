bf2ee8672e241cf9221e1bd429c9cf1bb3dea2f1
VanyaShumilo <ivan.shumilo@gmail.com>
2022-12-17 00:26:35 +0200

    loader

--- src/App.js
+++ src/App.js
@@ -1,61 +1,43 @@
 import PostItem from "./components/PostItem";
-import {useMemo, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/buttons/MyButton";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import {useSortedPosts} from "./components/hooks/usePosts";
-import axios from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
 
 
 function App() {
 
-    const [posts, setPosts] = useState([
-        {
-            id: 1,
-            title: "JavaScript",
-            body: "Programming language JS"
-        },
-        {
-            id: 2,
-            title: "JavaScript",
-            body: "Programming language JS"
-        },
-        {
-            id: 3,
-            title: "JavaScript",
-            body: "Programming language JS"
-        },
-        {
-            id: 4,
-            title: "aaa",
-            body: "zzz"
-        },
-        {
-            id: 5,
-            title: "zzz",
-            body: "aaa"
-        },
-    ])
+    const [posts, setPosts] = useState([])
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
     const [filter, setFilter] = useState({sort: '', query: ''})
     const sortedAndSearchedPosts = useSortedPosts(posts, filter.sort, filter.query)
     const [modalVisible, setModalVisible] = useState(false)
+    const [isPostsLoading, setIsPostsLoading] = useState(false)
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModalVisible(false)
     }
 
     async function fetchPosts() {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-        setPosts(response.data)
+        setIsPostsLoading(true)
+        const posts = await PostService.getAll()
+        setPosts(posts)
+        setIsPostsLoading(false)
+
     }
 
-    const [darkMode, setDarkMode] = useState(true)
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
+    const [darkMode, setDarkMode] = useState(true)
 
     return (
 
@@ -65,7 +47,8 @@ function App() {
                 <MyButton onClick={fetchPosts}>Load posts</MyButton>
                 <MyButton onClick={() => setModalVisible(true)}>Add Post</MyButton>
                 <div className={"toggleDarkMode"}>
-                    <input id={"darkMode"} name={"darkMode"} type="checkbox" checked={darkMode} onChange={() => setDarkMode(!darkMode)}/>
+                    <input id={"darkMode"} name={"darkMode"} type="checkbox" checked={darkMode}
+                           onChange={() => setDarkMode(!darkMode)}/>
                     <label htmlFor="darkMode">Dark mode</label>
                 </div>
                 <MyModal visible={modalVisible} setVisible={setModalVisible}>
@@ -79,7 +62,10 @@ function App() {
                     setFilter={setFilter}
                 />
                 {
-                    <PostList removePost={removePost} posts={sortedAndSearchedPosts} title={"Posts list about js"}/>
+                    isPostsLoading
+                        ? <div className="centerLoader"><Loader/></div>
+                        :
+                        <PostList removePost={removePost} posts={sortedAndSearchedPosts} title={"Posts list about js"}/>
                 }
 
             </div>
